c2b17bc42347b39a9067d7090bf8a7aa66d7bf50
USER <USER@DESKTOP-GUL41FM>
2021-11-27 00:34:38 +0300

    авторизация

--- app/src/main/java/com/example/shop/DBhelper.java
+++ app/src/main/java/com/example/shop/DBhelper.java
@@ -9,13 +9,18 @@ import androidx.annotation.Nullable;
 
 public class DBhelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 5;
+    public static final int DATABASE_VERSION = 6;
     public static final String DATABASE_NAME = "storeDB";
-    public static final String TABLE_PRODUCT = "contacts";//
+    public static final String TABLE_PRODUCT = "contacts";
+    public static final String TABLE_USERS = "usersTable";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_TITLE = "title";
     public static final String KEY_PRICE = "price";
+
+    public static final String KEY_IdUs = "id_User";
+    public static final String KEY_Name = "title";
+    public static final String KEY_Password = "price";
 
     public DBhelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -25,11 +30,18 @@ public class DBhelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_PRODUCT + "(" + KEY_ID
                 + " integer primary key," + " text," + KEY_TITLE + " text," + KEY_PRICE + " integer" + ")");
+
+
+        db.execSQL("create table " + TABLE_USERS + "("
+                + KEY_IdUs
+                + " integer primary key," + " text," + KEY_Name + " text," + KEY_Password + " text" + ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("drop table if exists " + TABLE_PRODUCT);
-        onCreate(db);
+            db.execSQL("drop table if exists " + TABLE_PRODUCT);
+            db.execSQL("drop table if exists " + TABLE_USERS);
+            onCreate(db);
+
     }
 }
